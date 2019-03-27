9a99afc83b7fb06220cd4e90528f272744388c1d
lukaspavelka <48788338+lukaspavelka@users.noreply.github.com>
2019-03-27 19:25:29 +0100

    Update dockertags.sh

--- dockertags.sh
+++ dockertags.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-# One line command : wget -q https://registry.hub.docker.com/v1/repositories/debian/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'
+# Build by Lukas Pavelka
 
 
 if [ $# -lt 1 ]
