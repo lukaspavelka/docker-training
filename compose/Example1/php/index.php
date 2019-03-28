54f8f1e27e606f170b047f8443a675928a51f03c
lukaspavelka <48788338+lukaspavelka@users.noreply.github.com>
2019-03-28 13:37:41 +0100

    Create index.php

---
+++ compose/Example1/php/index.php
@@ -0,0 +1,13 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <title>Hello from DOCKER World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, From DOCKER World!";
+        ?>
+    </body>
+</html>
